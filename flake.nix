9b88ce9fee3afa1ce0287b6b00d860f67a8c8d85
ashamedConsequence <maxime.morize@outlook.com>
2023-09-21 13:57:18 -0700

    think i added doom-emacs

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "Your new nix config";
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
+    # TODO: Add any other flake you might need
+    nixos-hardware.url = "github:nixos/nixos-hardware/master";
+
+    # Shameless plug: looking for a way to nixify your themes and make
+    # everything match nicely? Try nix-colors!
+    # nix-colors.url = "github:misterio77/nix-colors";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    nixos-hardware,
+    nix-doom-emacs,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+  in {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        # > Our main nixos configuration file <
+        modules = [ 
+	nixos-hardware.nixosModules.lenovo-thinkpad-t480
+	./nixos/configuration.nix
+	];
+      };
+    };
+
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations = {
+      "maximem@nixos" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        # > Our main home-manager configuration file <
+        modules = [./home-manager/home.nix];
+      };
+	imports = [ nix-doom-emacs.hmModule ];
+            programs.doom-emacs = {
+              enable = true;
+              doomPrivateDir = ./doom.d; # Directory containing your config.el, init.el
+                                         # and packages.el files
+            };
+
+    };
+  };
+}
